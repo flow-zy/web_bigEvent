774cac86394f585d81c78d1719437eb3b5f2f0d0
flow-zy <wz19121@yeah.net>
2022-04-27 18:23:14 +0800

    大事件项目

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,21 @@
-$(function() {
+$(function () {
   // 调用 getUserInfo 获取用户基本信息
   getUserInfo()
 
   var layer = layui.layer
 
   // 点击按钮，实现退出功能
-  $('#btnLogout').on('click', function() {
+  $('#btnLogout').on('click', function () {
     // 提示用户是否确认退出
-    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+    layer.confirm('确定退出登录?', {
+      icon: 3,
+      title: '提示'
+    }, function (index) {
       //do something
       // 1. 清空本地存储中的 token
       localStorage.removeItem('token')
       // 2. 重新跳转到登录页面
-      location.href = '/login.html'
+      location.href = './login.html'
 
       // 关闭 confirm 询问框
       layer.close(index)
@@ -25,7 +28,7 @@ function getUserInfo() {
   $.ajax({
     method: 'GET',
     url: '/my/userinfo',
-    success: function(res) {
+    success: function (res) {
       if (res.status !== 0) {
         return layui.layer.msg('获取用户信息失败！')
       }
